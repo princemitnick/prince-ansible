b77ff20af29d02f09896a47db90b1e0b763de039
princemitnick <ingjeanbaptiste@gmail.com>
2019-08-05 14:53:22 -0400

    Add new files

--- update101.sql
+++ update101.sql
@@ -1,2 +1,3 @@
-ALTER TABLE person ADD COLUMNleft_index_quality VARCHAR(15) NULL AFTER left_index;
+ALTER TABLE person ADD COLUMN left_index_quality VARCHAR(15) NULL AFTER left_index;
+
 ALTER TABLE person ADD COLUMN right_index_quality VARCHAR(15) NULL AFTER right_index;
