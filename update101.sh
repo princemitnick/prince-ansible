53bbbac5b42e1ce128b9b0ce97c776cfd46d7e63
princemitnick <ingjeanbaptiste@gmail.com>
2019-08-05 15:34:33 -0400

    Add new config files

--- update101.sh
+++ update101.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker exec -i mysql-kanj mysql -uroot -pmysql --uroot --pmysql --host 172.17.0.1 --port 3307 id < /opt/kanj/update101.sql
+docker exec -i mysql-kanj mysql -uroot -pmysql --host 172.17.0.1 --port 3307 id < /opt/kanj/update101.sql
 if [ $? -eq 0 ]
 then
     echo "Update done" >> /opt/kanj/update.log
